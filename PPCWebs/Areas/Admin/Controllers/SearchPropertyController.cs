0838475e00eedfa180843da60569783e5807814c
saladassvn <buiquocdat321@gmail.com>
2021-10-21 20:46:20 +0700

    Add chuc nang search bat dong san

---
+++ PPCWebs/Areas/Admin/Controllers/SearchPropertyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPCWebs.Models;
+
+namespace PPCWebs.Areas.Admin.Controllers
+{
+    public class SearchPropertyController : Controller
+    {
+        // GET: Admin/SearchProperty
+        AD25Team21Entities db = new AD25Team21Entities();
+        public ActionResult Index(string searching)
+        {
+            return View(db.Properties.Where(x => x.PropertyName.Contains(searching) || searching == null).ToList());
+        }
+    }
+}
