0c9712558d0a149c17a5dfe00ee59cbd50a62343
saladassvn <buiquocdat321@gmail.com>
2021-10-21 17:14:36 +0700

    day lan 2

---
+++ PPCWebs/Areas/Admin/Controllers/ProductAdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPCWebs.Models;
+
+namespace PPCWebs.Areas.Admin.Controllers
+{
+    public class ProductAdminController : Controller
+    {
+        AD25Team21Entities model = new AD25Team21Entities();
+        // GET: Admin/ProductAdmin
+        public ActionResult Index()
+        {
+            var property = model.Properties.OrderByDescending(x => x.ID).ToList();
+            return View(property);
+        }
+    }
+}
