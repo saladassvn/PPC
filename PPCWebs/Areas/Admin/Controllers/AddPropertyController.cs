ecbddf97fb350c3eb4fd5ad38ffdb1ba1c11e3bd
Nguyễn Chí Thành <classic.nct@gmail.com>
2021-10-24 09:25:15 +0700

    UI Add Property

---
+++ PPCWebs/Areas/Admin/Controllers/AddPropertyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PPCWebs.Areas.Admin.Controllers
+{
+    public class AddPropertyController : Controller
+    {
+        // GET: Admin/AddProperty
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
